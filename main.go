c4a95c541eccb3383148f5f91f4970f416044e8c
HaoDaWang <1306036576@qq.com>
2018-10-09 10:37:56 +0800

    Add info struct

--- main.go
+++ main.go
@@ -5,9 +5,22 @@ import (
 
 )
 
+type EnvironmentSDK struct {
+	sdk string
+}
+
+type PubSpec struct {
+	name string
+	description string
+	version string
+	environment EnvironmentSDK
+}
+
 func printString(content string){
-	fmt.Printf("%c[%d;%dm%s%c[0m\n",0x1B, 0, 34,content, 0x1B)
+	fmt.Printf("%c[%d;%dm%s%c[0m\n", 0x1B, 0, 34, content, 0x1B)
 }
 
 func main(){
+
 }
+
